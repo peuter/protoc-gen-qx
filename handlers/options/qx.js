6720193d83db829aa01b210078eb294a78cdcede
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2018-06-28 09:11:37 +0200

    fix indentation

--- handlers/options/qx.js
+++ handlers/options/qx.js
@@ -5,25 +5,25 @@ module.exports = {
   handler: function (options, propertyDefinition, context) {
     const props = propertyDefinition.entries
     if (options.hasOwnProperty('annotations') && options.annotations) {
-        addAnnotations(props, options.annotations.split(',').map(x => x.trim()))
-      }
-      if (options.hasOwnProperty('date') && options.date === true) {
-        setPropEntry(props, 'transform', `'_toDate'`)
-        setPropEntry(props, 'check', `'Date'`)
-        setPropEntry(props, 'init', `null`)
-        setPropEntry(props, 'nullable', `true`)
-        if (propertyDefinition.type.pbType === 'String') {
-          // use RFC 3339 format
-          propertyDefinition.writerTransform = `
+      addAnnotations(props, options.annotations.split(',').map(x => x.trim()))
+    }
+    if (options.hasOwnProperty('date') && options.date === true) {
+      setPropEntry(props, 'transform', `'_toDate'`)
+      setPropEntry(props, 'check', `'Date'`)
+      setPropEntry(props, 'init', `null`)
+      setPropEntry(props, 'nullable', `true`)
+      if (propertyDefinition.type.pbType === 'String') {
+        // use RFC 3339 format
+        propertyDefinition.writerTransform = `
       f = f instanceof Date ? f.toISOString() : ''${context.lineEnd}`
-        } else {
-          // use timestamp
-          propertyDefinition.writerTransform = `
+      } else {
+        // use timestamp
+        propertyDefinition.writerTransform = `
       f = f instanceof Date ? '' + Math.round(f.getTime() / 1000) : ''${context.lineEnd}`
-        }
       }
-      if (options.hasOwnProperty('validate') && options.validate) {
-        setPropEntry(props, `validate`, `${context.baseNamespace}.util.ValidatorFactory.getValidator('${options.validate}')`)
-      }
+    }
+    if (options.hasOwnProperty('validate') && options.validate) {
+      setPropEntry(props, `validate`, `${context.baseNamespace}.util.ValidatorFactory.getValidator('${options.validate}')`)
+    }
   }
 }
